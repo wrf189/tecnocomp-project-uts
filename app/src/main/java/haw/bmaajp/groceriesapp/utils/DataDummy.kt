d7cd28f6bf3a844ef625201fe189941a4d15c4be
Wangsa Fatahillah <wrfatahillah@gmail.com>
2023-11-17 16:36:55 +0700

    first commit

---
+++ app/src/main/java/haw/bmaajp/groceriesapp/utils/DataDummy.kt
@@ -0,0 +1,176 @@
+package haw.bmaajp.groceriesapp.utils
+
+import haw.bmaajp.groceriesapp.R
+import haw.bmaajp.groceriesapp.domain.model.AboutItem
+import haw.bmaajp.groceriesapp.domain.model.CategoryItem
+import haw.bmaajp.groceriesapp.domain.model.ProductItem
+import haw.bmaajp.groceriesapp.ui.theme.*
+
+object DataDummy {
+
+    fun generateDummyProduct(): List<ProductItem> {
+        return listOf(
+            ProductItem(
+                id = 1,
+                title = "Gigabyte GeForce RTX 4060 8GB GDDR6 Gaming OC-GV-N4060GAMING OC-8GD",
+                description = "Specifications, Chipset: GeForce RTX 4060, Core Clock: 2550 MHz (Reference card: 2460 MHz), CUDA Cores: 3072, Memory Clock: 17 Gbps, Memory Size: 8 GB, Memory Type: GDDR6, Memory Bus: 128 bit, Card Bus: PCI-E 4.0, Digital max resolution: 7680x4320, Multi-view: 4, Card size: L=281 W=114 H=40 mm, PCB Form: ATX, DirectX: 12 Ultimate",
+                image = R.drawable.rtx4060,
+                unit = "1pcs, Price",
+                price = 375.0,
+                nutritions = "Gigabyte",
+                review = 3.5
+            ),
+            ProductItem(
+                id = 2,
+                title = "Gigabyte GeForce RTX 4060 8GB GDDR6 Gaming OC-GV-N4060GAMING OC-8GD",
+                description = "Specifications, Chipset: GeForce RTX 4060, Core Clock: 2550 MHz (Reference card: 2460 MHz), CUDA Cores: 3072, Memory Clock: 17 Gbps, Memory Size: 8 GB, Memory Type: GDDR6, Memory Bus: 128 bit, Card Bus: PCI-E 4.0, Digital max resolution: 7680x4320, Multi-view: 4, Card size: L=281 W=114 H=40 mm, PCB Form: ATX, DirectX: 12 Ultimate",
+                image = R.drawable.rtx4060,
+                unit = "1pcs, Price",
+                price = 375.0,
+                nutritions = "Gigabyte",
+                review = 3.5
+            ),
+            ProductItem(
+                id = 3,
+                title = "Gigabyte GeForce RTX 4060 8GB GDDR6 Gaming OC-GV-N4060GAMING OC-8GD",
+                description = "Specifications, Chipset: GeForce RTX 4060, Core Clock: 2550 MHz (Reference card: 2460 MHz), CUDA Cores: 3072, Memory Clock: 17 Gbps, Memory Size: 8 GB, Memory Type: GDDR6, Memory Bus: 128 bit, Card Bus: PCI-E 4.0, Digital max resolution: 7680x4320, Multi-view: 4, Card size: L=281 W=114 H=40 mm, PCB Form: ATX, DirectX: 12 Ultimate",
+                image = R.drawable.rtx4060,
+                unit = "1pcs, Price",
+                price = 375.0,
+                nutritions = "Gigabyte",
+                review = 3.5
+            ),
+            ProductItem(
+                id = 4,
+                title = "Gigabyte GeForce RTX 4060 8GB GDDR6 Gaming OC-GV-N4060GAMING OC-8GD",
+                description = "Specifications, Chipset: GeForce RTX 4060, Core Clock: 2550 MHz (Reference card: 2460 MHz), CUDA Cores: 3072, Memory Clock: 17 Gbps, Memory Size: 8 GB, Memory Type: GDDR6, Memory Bus: 128 bit, Card Bus: PCI-E 4.0, Digital max resolution: 7680x4320, Multi-view: 4, Card size: L=281 W=114 H=40 mm, PCB Form: ATX, DirectX: 12 Ultimate",
+                image = R.drawable.rtx4060,
+                unit = "1pcs, Price",
+                price = 375.0,
+                nutritions = "Gigabyte",
+                review = 3.5
+            ),
+            ProductItem(
+                id = 5,
+                title = "Gigabyte GeForce RTX 4060 8GB GDDR6 Gaming OC-GV-N4060GAMING OC-8GD",
+                description = "Specifications, Chipset: GeForce RTX 4060, Core Clock: 2550 MHz (Reference card: 2460 MHz), CUDA Cores: 3072, Memory Clock: 17 Gbps, Memory Size: 8 GB, Memory Type: GDDR6, Memory Bus: 128 bit, Card Bus: PCI-E 4.0, Digital max resolution: 7680x4320, Multi-view: 4, Card size: L=281 W=114 H=40 mm, PCB Form: ATX, DirectX: 12 Ultimate",
+                image = R.drawable.rtx4060,
+                unit = "1pcs, Price",
+                price = 375.0,
+                nutritions = "Gigabyte",
+                review = 3.5
+            ),
+            ProductItem(
+                id = 6,
+                title = "Gigabyte GeForce RTX 4060 8GB GDDR6 Gaming OC-GV-N4060GAMING OC-8GD",
+                description = "Specifications, Chipset: GeForce RTX 4060, Core Clock: 2550 MHz (Reference card: 2460 MHz), CUDA Cores: 3072, Memory Clock: 17 Gbps, Memory Size: 8 GB, Memory Type: GDDR6, Memory Bus: 128 bit, Card Bus: PCI-E 4.0, Digital max resolution: 7680x4320, Multi-view: 4, Card size: L=281 W=114 H=40 mm, PCB Form: ATX, DirectX: 12 Ultimate",
+                image = R.drawable.rtx4060,
+                unit = "1pcs, Price",
+                price = 375.0,
+                nutritions = "Gigabyte",
+                review = 3.5
+            ),
+            ProductItem(
+                id = 7,
+                title = "Gigabyte GeForce RTX 4060 8GB GDDR6 Gaming OC-GV-N4060GAMING OC-8GD",
+                description = "Specifications, Chipset: GeForce RTX 4060, Core Clock: 2550 MHz (Reference card: 2460 MHz), CUDA Cores: 3072, Memory Clock: 17 Gbps, Memory Size: 8 GB, Memory Type: GDDR6, Memory Bus: 128 bit, Card Bus: PCI-E 4.0, Digital max resolution: 7680x4320, Multi-view: 4, Card size: L=281 W=114 H=40 mm, PCB Form: ATX, DirectX: 12 Ultimate",
+                image = R.drawable.rtx4060,
+                unit = "1pcs, Price",
+                price = 375.0,
+                nutritions = "Gigabyte",
+                review = 3.5
+            ),
+            ProductItem(
+                id = 8,
+                title = "Gigabyte GeForce RTX 4060 8GB GDDR6 Gaming OC-GV-N4060GAMING OC-8GD",
+                description = "Specifications, Chipset: GeForce RTX 4060, Core Clock: 2550 MHz (Reference card: 2460 MHz), CUDA Cores: 3072, Memory Clock: 17 Gbps, Memory Size: 8 GB, Memory Type: GDDR6, Memory Bus: 128 bit, Card Bus: PCI-E 4.0, Digital max resolution: 7680x4320, Multi-view: 4, Card size: L=281 W=114 H=40 mm, PCB Form: ATX, DirectX: 12 Ultimate",
+                image = R.drawable.rtx4060,
+                unit = "1pcs, Price",
+                price = 375.0,
+                nutritions = "Gigabyte",
+                review = 3.5
+            ),
+            ProductItem(
+                id = 9,
+                title = "Gigabyte GeForce RTX 4060 8GB GDDR6 Gaming OC-GV-N4060GAMING OC-8GD",
+                description = "Specifications, Chipset: GeForce RTX 4060, Core Clock: 2550 MHz (Reference card: 2460 MHz), CUDA Cores: 3072, Memory Clock: 17 Gbps, Memory Size: 8 GB, Memory Type: GDDR6, Memory Bus: 128 bit, Card Bus: PCI-E 4.0, Digital max resolution: 7680x4320, Multi-view: 4, Card size: L=281 W=114 H=40 mm, PCB Form: ATX, DirectX: 12 Ultimate",
+                image = R.drawable.rtx4060,
+                unit = "1pcs, Price",
+                price = 375.0,
+                nutritions = "Gigabyte",
+                review = 3.5
+            ),
+            ProductItem(
+                id = 10,
+                title = "Gigabyte GeForce RTX 4060 8GB GDDR6 Gaming OC-GV-N4060GAMING OC-8GD",
+                description = "Specifications, Chipset: GeForce RTX 4060, Core Clock: 2550 MHz (Reference card: 2460 MHz), CUDA Cores: 3072, Memory Clock: 17 Gbps, Memory Size: 8 GB, Memory Type: GDDR6, Memory Bus: 128 bit, Card Bus: PCI-E 4.0, Digital max resolution: 7680x4320, Multi-view: 4, Card size: L=281 W=114 H=40 mm, PCB Form: ATX, DirectX: 12 Ultimate",
+                image = R.drawable.rtx4060,
+                unit = "1pcs, Price",
+                price = 375.0,
+                nutritions = "Gigabyte",
+                review = 3.5
+            ),
+        )
+    }
+
+    fun generateDummyCategories(): List<CategoryItem> {
+        return listOf(
+            CategoryItem(
+                title = "Aksesoris Komputer",
+                image = R.drawable.aksesoris,
+                background = BackgroundCategory1
+            ),
+            CategoryItem(
+                title = "Aksesoris Komputer",
+                image = R.drawable.aksesoris,
+                background = BackgroundCategory1
+            ),
+            CategoryItem(
+                title = "Aksesoris Komputer",
+                image = R.drawable.aksesoris,
+                background = BackgroundCategory1
+            ),
+            CategoryItem(
+                title = "Aksesoris Komputer",
+                image = R.drawable.aksesoris,
+                background = BackgroundCategory1
+            ),
+            CategoryItem(
+                title = "Aksesoris Komputer",
+                image = R.drawable.aksesoris,
+                background = BackgroundCategory1
+            ),
+            CategoryItem(
+                title = "Aksesoris Komputer",
+                image = R.drawable.aksesoris,
+                background = BackgroundCategory1
+            )
+        )
+    }
+
+    fun generateDummyAbout(): List<AboutItem> {
+        return listOf(
+            AboutItem(
+                image = R.drawable.baseline_android_24,
+                title = "About Us"
+            ),
+            AboutItem(
+                image = R.drawable.baseline_groups_24,
+                title = "Kelompok 4 BM5B",
+            ),
+            AboutItem(
+                image = R.drawable.asrul,
+                title = "Asrul Hafidz R 2103421020"
+            ),
+            AboutItem(
+                image = R.drawable.rofi,
+                title = "Jilaan Rofi Arkaan 2103421036"
+            ),
+            AboutItem(
+                image = R.drawable.wangsa,
+                title = "Wangsa Reisyah F 2103421005"
+            ),
+        )
+    }
+
+}
